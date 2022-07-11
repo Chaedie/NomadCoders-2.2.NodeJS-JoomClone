09daec2a6781085817342de5f5af752c7dc4de45
Chaedie <chaedong.im.dev@gmail.com>
2022-07-11 16:02:53 +0900

    learn : ws 라이브러리를 통해 "WebSocket 서버 열기" 학습

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -8,5 +10,15 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
-app.listen(PORT, console.log(`✅ Listen "http://localhost:${PORT}/"`));
+const handleListen = () => console.log(`✅ Listen "http://localhost:${PORT}/"`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on('connection', handleConnection);
+
+server.listen(PORT, handleListen);
